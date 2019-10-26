a5ccf89b54af1acebf533dba8945f685d696158d
yyspyysp <45526192+yyspyysp@users.noreply.github.com>
2019-10-26 09:27:45 +0800

    增加子弹

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,13 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour {
+public class Bullet : MonoBehaviour
+{
+   public int t = 0;
+    private Rigidbody rigidbody;
     
-	void Start () {
-		
+	void Start ()
+	{
+	    rigidbody = this.GetComponent<Rigidbody>();
 	}
 	
 	void Update () {
-		
+
+	    if (t > 0)
+	    {
+            rigidbody.velocity=new Vector3(-10,0,0);
+	    }
+	    else
+	    {
+	        rigidbody.velocity=new Vector3(10,0,0);
+	    }
+	 
+	 
 	}
 }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,14 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
-public class Weapon : MonoBehaviour {
-    
+public class Weapon : MonoBehaviour
+{
+    public GameObject[] Bullet;
+    public int BulletNum = 0;
+    public GameObject weaponRight;
+    public GameObject weaponLeft;
+   
 	void Start () {
 		
 	}
 	
 	void Update () {
-		
+		Shoot();
+        SetBullet();
 	}
+
+    void Shoot()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (weaponRight.transform.position.x > weaponLeft.transform.position.x)
+            {
+                GameObject g = Instantiate(Bullet[BulletNum], weaponRight.transform.position, Bullet[BulletNum].transform.rotation);
+                BulletNum++;
+            }
+            else
+            {
+               
+               GameObject g= Instantiate(Bullet[BulletNum], weaponRight.transform.position, Bullet[BulletNum].transform.rotation);
+                //g.transform.rotation = new Quaternion(g.transform.rotation.x, g.transform.rotation.y ,g.transform.rotation.z+90, 0);
+                BulletNum++;
+
+            }
+
+            if (BulletNum == 7)
+            {
+                BulletNum = 0;
+            }
+        }
+    }
+
+    void SetBullet()
+    {
+        if (weaponRight.transform.position.x > weaponLeft.transform.position.x)
+        {
+            Bullet[BulletNum].GetComponent<Bullet>().t = -1;
+           
+            Debug.Log("you");
+        }
+        else
+        {
+           Bullet[BulletNum].GetComponent<Bullet>().t = 1;
+            Debug.Log("zuo");
+        }
+    }
 }
