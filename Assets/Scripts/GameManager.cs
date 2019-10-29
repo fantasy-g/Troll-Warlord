96de922737b02ff1111e51ec2ea8e08652f1cb10
wangtianci <377090139@qq.com>
2019-10-29 20:43:01 +0800

    游戏结束

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,12 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
-    
-	void Start () {
-		
+
+    private static GameManager _instance;
+    public static GameManager Instance { get { return _instance; } }
+
+    public float deadposy = -4.2f;
+    public GameObject replay;
+    public GameObject deadtext;
+    public bool PlayerisDead = false;
+
+
+    private void Awake()
+    {
+        if(_instance==null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            return;
+        }
+    }
+    void Start () {
 	}
 	
 	void Update () {
 		
 	}
+
+
+    public void GameOver()
+    {
+        if(PlayerisDead==false)
+        {
+            PlayerisDead = true;
+            replay.SetActive(true);
+            deadtext.SetActive(true);
+        }
+    }
+
+    public void SetPlayerDead()
+    {
+        if (PlayerisDead == true)
+            PlayerisDead = false;
+        Debug.Log("yes");
+    }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,12 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-    
+
+    private float DeadPosY;
+
 	void Start () {
-		
+        DeadPosY = GameManager.Instance.deadposy;
 	}
 	
 	void Update () {
-		
+        DeadCheck();
 	}
+
+
+    void DeadCheck()
+    {
+        if(transform.position.y<DeadPosY)
+        {
+            GameManager.Instance.GameOver();
+        }
+
+    }
+
+
 }
